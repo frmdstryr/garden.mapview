ce06aed949625cec54ea1ae725eb805b0ae846d6
frmdstryr <frmdstryr@gmail.com>
2016-07-21 02:00:18 -0400

    And again...

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup, Extension
 #from Cython.Build import cythonize
-extensions = [Extension("*", ["mapview/*.pyx"])]
+extensions = [
+    Extension("mapview.source", ["mapview/source.pyx"]),
+    Extension("mapview.types", ["mapview/types.pyx"]),
+    Extension("mapview.utils", ["mapview/utils.pyx"]),
+    Extension("mapview.view", ["mapview/view.pyx"]),
+    Extension("mapview.widgets", ["mapview/widgets.pyx"]),
+]
 setup(
     name = 'Cythonized MapView',
     packages = ['mapview'],
